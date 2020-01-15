44b801270d271ba33979d665fc2b51998fa276e8
Tuấn <duongquoctuan8999@gmail.com>
2020-01-15 15:14:33 +0700

    Add files via upload

---
+++ BT0102/BT05/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c;
+
+            Console.Write("Nhap canh a: ");
+            a = float.Parse(Console.ReadLine());
+
+            Console.Write("Nhap canh b: ");
+            b = float.Parse(Console.ReadLine());
+
+            Console.Write("Nhap canh c: ");
+            c = float.Parse(Console.ReadLine());
+
+            if (laTamGiac(a, b, c) == 1)
+            {
+                if (laTamGiacVuongCan(a, b, c) == 1)
+                {
+                    Console.WriteLine("La tam giac VUONG CAN!!");
+                }
+                else if (laTamGiacDeu(a, b, c) == 1)
+                {
+                    Console.WriteLine("La tam giac DEU!!");
+                }
+                else if (laTamGiacVuong(a, b, c) == 1)
+                {
+                    Console.WriteLine("La tam giac VUONG!!");
+                }
+                else if (laTamGiacCan(a, b, c) == 1)
+                {
+                    
+                    Console.WriteLine("La tam giac CAN");
+                }
+                else
+                {
+                    Console.WriteLine("La tam giac THUONG!!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Khong phai la tam giac!!");
+            }
+            Console.ReadKey();
+        }
+        static int laTamGiac(float a, float b, float c)
+        {
+            if (a + b > c || a + c > b || b + c > a) return 1;
+            else return 0;          
+        }
+        static int laTamGiacCan(float a, float b, float c)
+        {
+            if (a == b || a == c || b == c)  return 1;
+            else return 0;
+        }
+        static int laTamGiacDeu(float a, float b, float c)
+        {
+            if (a == b && b == c) return 1;
+            else return 0;
+        }
+        static int laTamGiacVuong(float a, float b, float c)
+        {
+            if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b) return 1;
+            else return 0;
+        }
+        static int laTamGiacVuongCan(float a, float b, float c)
+        {
+            if (laTamGiacVuong(a, b, c) == 1 && laTamGiacCan(a, b, c) == 1) return 1;
+            else return 0;
+        }
+    }
+}
---
+++ BT0102/BT06/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            do
+            {
+                Console.Write("Nhap a: ");
+                a = int.Parse(Console.ReadLine());
+            } while (a < 0);
+
+            do
+            {
+                Console.Write("Nhap b: ");
+                b = int.Parse(Console.ReadLine());
+            } while (b < 0);
+            do
+            {
+                Console.Write("Nhap c: ");
+                c = int.Parse(Console.ReadLine());
+            } while (c < 0);
+            do
+            {
+                Console.Write("Nhap d: ");
+                d = int.Parse(Console.ReadLine());
+            } while (d < 0);
+           
+            Console.WriteLine("So lon nhat la: {0}", Math.Max(Math.Max(a, b), Math.Max(c, d)));
+
+            Console.ReadKey();
+
+        }
+    }
+}
