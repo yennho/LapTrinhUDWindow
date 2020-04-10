5eb9920ed4e562aa1622a002e6720ed953d783e8
Tuan <duongquoctuan8999@gmail.com>
2020-04-10 18:55:12 +0700

    t

---
+++ BaiTap/BT0102/BT0102/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT0102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double diemToan, diemLy, diemHoa;
+            Console.Write("Nhap diem toan: ");
+            diemToan = Double.Parse(Console.ReadLine());
+
+            Console.Write("Nhap diem ly: ");
+            diemLy = Double.Parse(Console.ReadLine());
+
+            Console.Write("Nhap diem hoa: ");
+            diemHoa = Double.Parse(Console.ReadLine());
+
+            Double diemTB = (diemToan + diemLy + diemHoa) / 3;
+
+            Console.WriteLine("Toan: {0}", diemToan);
+            Console.WriteLine("Ly: {0}", diemLy);
+            Console.WriteLine("Hoa: {0}", diemHoa);
+            Console.WriteLine("TBM: {0}", diemTB);
+            
+            if(diemTB < 5)
+            {
+                Console.WriteLine("Xep loai: Yeu");
+            }
+            else if (diemTB >=5 && diemTB < 6.5)
+            {
+                Console.WriteLine("Xep loai: Trung Binh");
+            }
+            else if (diemTB >= 6.5 && diemTB < 8)
+            {
+                Console.WriteLine("Xep loai: Kha");
+            }
+            else
+            {
+                Console.WriteLine("Xep loai: Gioi");
+            }
+
+            Console.ReadKey();            
+        }
+    }
+}
---
+++ BaiTap/BT0102/BT08/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, luaChon, re;
+            Console.Write("Nhap so a: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap so b: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("Cong:1\nTru:2\nNhan:3\nChia:4\nNhap lua chon: ");
+            luaChon = int.Parse(Console.ReadLine());
+
+            switch (luaChon)
+            {
+                case 1:
+                    re = a + b;
+                    Console.WriteLine("{0} + {1} = {2}", a, b, re);
+                    break;
+                case 2:
+                    re = a - b;
+                    Console.WriteLine("{0} - {1} = {2}", a, b, re);
+                    break;
+                case 3:
+                    re = a * b;
+                    Console.WriteLine("{0} * {1} = {2}", a, b, re);
+                    break;
+                case 4:
+                    float re4;
+                    re4 = (float)a / (float)b;
+                    Console.WriteLine("{0} / {1} = {2}", a, b, re4);
+                    break;
+                default:
+                    Console.WriteLine("Phep toan khong hop le!");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
